9ac8b3bf77d8e86429554e17cb6efded2e994b2e
mluquet <mluquet@gmail.com>
2015-05-27 23:00:43 +0200

    ajout dossier db

---
+++ db/migrate/20150525141117_create_liens.rb
@@ -0,0 +1,14 @@
+﻿class CreateLiens < ActiveRecord::Migration
+  def change
+  	create_table :liens do |t|
+  		t.string :nom
+  		t.string :url
+  		t.string :description
+  		t.string :profil
+  		t.string :domaine
+  		
+  		t.timestamps
+  	end
+  end
+end
+
